5de386c105c2a70f35b9cf816987100bab348510
Santin Liam <94681785+liam-santin@users.noreply.github.com>
2023-05-04 15:25:48 +0200

    ajout routeur

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,6 +1,21 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+ import Home from "@/pages/Home.vue";
+import CinematiqueDebut from "@/pages/cinématique-début.vue";
+import {createWebHistory, createRouter} from "vue-router";
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        { path: '/', component: Home, name: "home" },
+        { path: '/cinématique', component: CinematiqueDebut, name: "cinematique" },
+    ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
