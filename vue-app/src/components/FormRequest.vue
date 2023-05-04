fe0e6a8f7e29f1ddb8e6ff5dec3cd2c88ba76c7f
Luca Pinto <pintoluca106@gmail.com>
2023-05-04 15:35:15 +0200

    supprimer branche sprint1

--- vue-app/src/components/FormRequest.vue
+++ vue-app/src/components/FormRequest.vue
@@ -4,12 +4,19 @@
 
 <template>
     <form method="get">
-        <p>Entrez la requête :</p>
-        <textarea id="requete" placeholder="Votre requête ici"></textarea>
-        <button @click="">Envoyer</button>
+        <div class="formulaire">
+            <p>Entrez la requête :</p>
+            <textarea id="requete" placeholder="Votre requête ici"></textarea>
+            <button @click="">Envoyer</button>
+        </div>
+
     </form>
 </template>
 
 <style scoped>
-
+.formulaire {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
 </style>
