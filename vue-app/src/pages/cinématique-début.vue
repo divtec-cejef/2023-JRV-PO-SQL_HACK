da0ec9edb9ade09186209b00dfce9d56c8abfa84
Santin Liam <94681785+liam-santin@users.noreply.github.com>
2023-05-04 15:38:58 +0200

    insertion de l'image dans la page cinématique-début.vue

--- vue-app/src/pages/cinématique-début.vue
+++ vue-app/src/pages/cinématique-début.vue
@@ -1,17 +1,22 @@
 <template>
+
   <div class="image-fullscreen">
     <img src="src/img/photo-cinématique-1.png" alt="">
     <p>essais</p>
     <RouterLink to="/">retour</RouterLink>
   </div>
+
+  <div class="test">test</div>
+
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+
 .image-fullscreen{
-  width: 100%;
-  height: 100%;
+
 }
+
 </style>
