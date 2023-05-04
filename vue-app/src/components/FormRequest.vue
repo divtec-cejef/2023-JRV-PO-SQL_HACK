1f7bb75747b58ccc370088dcf71035334ddde2be
Luca Pinto <pintoluca106@gmail.com>
2023-05-04 13:37:56 +0200

    form requête

---
+++ vue-app/src/components/FormRequest.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <form method="get">
+        <p>Entrez la requête :</p>
+        <textarea id="requete" placeholder="Votre requête ici"></textarea>
+        <button @click="">Envoyer</button>
+    </form>
+</template>
+
+<style scoped>
+
+</style>
