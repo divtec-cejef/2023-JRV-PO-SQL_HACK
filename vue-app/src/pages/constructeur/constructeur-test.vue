76ea06b9ec52541a761ce13e79ace213bc569cb3
Santin Liam <94681785+liam-santin@users.noreply.github.com>
2023-05-08 09:16:32 +0200

    création de la page 2 avec les bouton dans les tables

--- vue-app/src/pages/constructeur/constructeur-test.vue
+++ vue-app/src/pages/constructeur/constructeur-test.vue
@@ -1,16 +1,35 @@
 <template>
-  <div class="constructeur">
-    <div class="bouton">
-      <button>SELECT</button>
-      <button>UPDATE</button>
-      <button>INSERT</button>
-      <button>DELETE</button>
+    <div class="page1">
+      <button @click="nextPageSelect">SELECT</button>
+      <button id="btn_update">UPDATE</button>
+      <button id="btn_insert">INSERT</button>
+      <button id="btn_delete">DELETE</button>
     </div>
-  </div>
+    <div class="page2">
+      <div class="table_voiture">
+        <button>idVoiture</button>
+        <button>couleur</button>
+        <button>propriétaire</button>
+        <button>numéroPlaque</button>
+      </div>
+      <div class="table_personnel">
+        <button>idPersonnel</button>
+        <button>Nom</button>
+        <button>Prénom</button>
+        <button>Datenaissance</button>
+        <button>Numéro de tel</button>
+      </div>
+      <div class="table_materiel">
+        <button>idMatériel</button>
+        <button>Nom</button>
+        <button>Quantité</button>
+      </div>
+    </div>
 </template>
 
 <script setup>
-
+  function nextPageSelect(){
+  }
 </script>
 
 <style scoped>
@@ -18,19 +37,44 @@
   margin: 0;
   border: red solid 2px;
 }
-.constructeur{
-  background-color: blue;
-  width: fit-content;
-  padding: 3em;
+
+/* page 1 */
+.page1 {
+  width: 500px;
+  background-color: aqua;
+  padding: 30px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 }
 
-.constructeur button{
+.page1 button{
   border: none;
-  font-size: 22px;
+  font-size: 26px;
   margin: 20px;
   padding: 1em;
+  border-radius: 20px;
 }
-.bouton {
-  background-color: aqua;
+
+button{
+  transition: transform 500ms ease;
 }
+button:hover{
+  transform: scale(1.1);
+}
+
+
+/* page 2 */
+.page2{
+  width: fit-content;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+
 </style>
