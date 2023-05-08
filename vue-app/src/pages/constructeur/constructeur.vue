26dc6cbb55458e3166bc0d8d1b38fc491cde45be
Santin Liam <94681785+liam-santin@users.noreply.github.com>
2023-05-08 14:17:52 +0200

    ajout d'une branche de test pour emettre le clic a un autre composant

--- vue-app/src/pages/constructeur/constructeur.vue
+++ vue-app/src/pages/constructeur/constructeur.vue
@@ -1,7 +1,9 @@
 <template>
-  <constructeur4-bouton :select="maFonction"></constructeur4-bouton>
+  <constructeur4-bouton @select="maFonction"></constructeur4-bouton>
   <constructeur-table> </constructeur-table>
   <constructeur-requete :text="text"></constructeur-requete>
+  <parent></parent>
+  <enfant></enfant>
 
   <button @click="ecrire">Ecrire</button>
 
@@ -11,7 +13,10 @@
 import Constructeur4Bouton from "@/pages/constructeur/constructeur-4-bouton.vue";
 import ConstructeurTable from "@/pages/constructeur/constructeur-table.vue";
 import ConstructeurRequete from "@/pages/constructeur/constructeur-requete.vue"
+
+
 import {ref} from "vue";
+import Parent from "@/pages/constructeur/test/parent.vue";
 
 function maFonction(valeur){
   console.log(valeur)
---
+++ vue-app/src/pages/constructeur/test/parent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <enfant @mon-evenement="maFonction"></enfant>
+  </div>
+</template>
+
+<script setup>
+import enfant from './enfant.vue';
+
+function maFonction(valeur) {
+  console.log(valeur); // Affiche "hello" dans la console
+  // Appeler la fonction souhaitée avec la valeur en tant qu'argument
+}
+</script>
