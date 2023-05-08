fb1ebc9bf9188e4a4d72d9a8ea05a749ec015fc5
Santin Liam <94681785+liam-santin@users.noreply.github.com>
2023-05-08 14:24:12 +0200

    quand on clique sur le bouton SELECT il y a le mot "select" qui vient s'afficher dans l'input

--- vue-app/src/pages/constructeur/constructeur.vue
+++ vue-app/src/pages/constructeur/constructeur.vue
@@ -1,12 +1,12 @@
 <template>
   <constructeur4-bouton @select="maFonction"></constructeur4-bouton>
   <constructeur-table> </constructeur-table>
-  <constructeur-requete :text="text"></constructeur-requete>
+  <constructeur-requete></constructeur-requete>
   <parent></parent>
   <enfant></enfant>
 
-  <button @click="ecrire">Ecrire</button>
-
+  <label for="text-requete">Texte en lecture seule</label>
+  <input type="text" id="text-requete" :value="text" readonly>
 </template>
 
 <script setup>
@@ -18,8 +18,9 @@ import ConstructeurRequete from "@/pages/constructeur/constructeur-requete.vue"
 import {ref} from "vue";
 import Parent from "@/pages/constructeur/test/parent.vue";
 
+const text = ref()
 function maFonction(valeur){
-  console.log(valeur)
+  text.value = valeur
 }
 </script>
 
