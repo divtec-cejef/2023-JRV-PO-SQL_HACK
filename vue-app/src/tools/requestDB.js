1de2677b35e6909dd48426b00ef35a6894586e69
Luca Pinto <pintoluca106@gmail.com>
2023-06-15 08:55:39 +0200

    assembler constructeur avec l'envoi de la requête

--- vue-app/src/tools/requestDB.js
+++ vue-app/src/tools/requestDB.js
@@ -1,23 +1,25 @@
 // const requestSaisie = '';
 
+
 /**
  * Récupère les mots de la chaine de caractère.
- * @param chaine La chaine de caractère à traiter.
+ * @param request La chaine de caractère à traiter.
  * @param index Nombre qui définit le mot sélectionné en fonction de son emplacement dans la chaine.
  * @returns {*} le mot sélectionné de la chaine.
  */
-function getWord(chaine, index) {
-    const mots = chaine.split(' ');
+function getWord(request, index) {
+    console.info(request);
+    const mots = request.split(' ');
     return mots[index];
 }
 
 /**
  * Compte le nombre de mots qu'il y a dans la chaine de caractère.
- * @param chaine La chaine de caractère à traiter.
+ * @param request La chaine de caractère à traiter.
  * @returns {*} le nombre de mots.
  */
-function numberWordInRequest(chaine) {
-    const mots = chaine.split(' ');
+function numberWordInRequest(request) {
+    const mots = request.split(' ');
     const nbreMots = mots.length;
     console.log(nbreMots);
     return nbreMots;
@@ -26,11 +28,11 @@ function numberWordInRequest(chaine) {
 /**
  * Récupère toutes les informations nécessaires pour exécuter une requête SELECT.
  */
-function executeSelectRequest(requestSaisie) {
-    const table = getWord(requestSaisie.value, 3).replace(/"|'|;|/g, "");
-    if (numberWordInRequest(requestSaisie.value) > 5 ) {
-        const champsCondition = getWord(requestSaisie.value, 5);
-        const valeur = getWord(requestSaisie.value, 7).replace(/"|'|;|/g, "");
+function executeSelectRequest(request) {
+    const table = getWord(request, 3).replace(/"|'|;|/g, "");
+    if (numberWordInRequest(request) > 5 ) {
+        const champsCondition = getWord(request, 5);
+        const valeur = getWord(request, 7).replace(/"|'|;|/g, "");
         console.log('table : ' + table + ', champs de condition : ' + champsCondition + ', valeur : ' + valeur);
         select(table, champsCondition, valeur);
     } else {
@@ -42,11 +44,11 @@ function executeSelectRequest(requestSaisie) {
 /**
  * Récupère toutes les informations nécessaires pour exécuter une requête INSERT.
  */
-function executeInsertRequest(requestSaisie) {
-    const table = getWord(requestSaisie.value, 2);
+function executeInsertRequest(request) {
+    const table = getWord(request, 2);
     const dataArray = ref([]);
-    for (let i = 5; i <= numberWordInRequest(requestSaisie.value) - 1; i++) {
-        const dataClean = getWord(requestSaisie.value, i).replace(/"|,|;|\(|\)/g, "");
+    for (let i = 5; i <= numberWordInRequest(request) - 1; i++) {
+        const dataClean = getWord(request, i).replace(/"|,|;|\(|\)/g, "");
         dataArray.value.push(dataClean);
     }
     console.log(table, ',', dataArray.value);
@@ -56,11 +58,11 @@ function executeInsertRequest(requestSaisie) {
 /**
  * Récupère toutes les informations nécessaires pour exécuter une requête UPDATE.
  */
-function executeUpdateRequest(requestSaisie) {
-    const table = getWord(requestSaisie.value, 1);
-    const champsModif = getWord(requestSaisie.value, 3);
-    const nouvelleValeur = getWord(requestSaisie.value, 5).replace(/"|'|;|/g, "");
-    const valeurID = getWord(requestSaisie.value, 9);
+function executeUpdateRequest(request) {
+    const table = getWord(request, 1);
+    const champsModif = getWord(request, 3);
+    const nouvelleValeur = getWord(request, 5).replace(/"|'|;|/g, "");
+    const valeurID = getWord(request, 9);
     console.log(table + ', ' + champsModif + ', ' + nouvelleValeur + ', ' + valeurID);
     update(table, parseInt(valeurID), champsModif, nouvelleValeur);
 }
@@ -68,10 +70,10 @@ function executeUpdateRequest(requestSaisie) {
 /**
  * Récupère toutes les informations nécessaires pour exécuter une requête DELETE.
  */
-function executeDeleteRequest(requestSaisie) {
-    const table = getWord(requestSaisie.value, 2);
-    const champsID = getWord(requestSaisie.value, 4);
-    const valeurID = getWord(requestSaisie.value, 6);
+function executeDeleteRequest(request) {
+    const table = getWord(request, 2);
+    const champsID = getWord(request, 4);
+    const valeurID = getWord(request, 6);
     console.log(table, champsID, valeurID);
     Delete(table, parseInt(valeurID));
 }
@@ -79,16 +81,17 @@ function executeDeleteRequest(requestSaisie) {
 /**
  * Choisit la bonne requête à exécuter en fonction du premier mot de la chaine de caractère.
  */
-function sendRequest(requestSaisie) {
-    const nomRequete = getWord(requestSaisie.value, 0).toUpperCase();
+export function sendRequest(request) {
+    console.log(request)
+    const nomRequete = getWord(request, 0).toUpperCase();
     switch (nomRequete) {
         case 'SELECT':
-            return executeSelectRequest(requestSaisie);
+            return executeSelectRequest(request);
         case 'UPDATE':
-            return executeUpdateRequest(requestSaisie);
+            return executeUpdateRequest(request);
         case 'INSERT':
-            return executeInsertRequest(requestSaisie);
+            return executeInsertRequest(request);
         case 'DELETE':
-            return executeDeleteRequest(requestSaisie);
+            return executeDeleteRequest(request);
     }
 }
