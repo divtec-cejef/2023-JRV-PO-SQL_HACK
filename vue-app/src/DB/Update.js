fea20374b319bfa046af5ff5c768eda7c28ab3f2
Evan Zuber <94693033+Evan-0425@users.noreply.github.com>
2023-08-23 14:53:28 +0200

    Oublie parathese

--- vue-app/src/DB/Update.js
+++ vue-app/src/DB/Update.js
@@ -12,66 +12,66 @@ function update(tableName,  id, propriete, updatedData) {
     };
 
 // Définissez la fonction de mise à jour
-function update(tableName, id, propriete, updatedData) {
-    let request = window.indexedDB.open("maBaseDeDonnees");
-    request.onsuccess = function (event) {
-        console.log("Ouverture de la base de données");
-        let db = event.target.result;
-        let transaction = db.transaction([tableName], "readwrite");
-        let objectStore = transaction.objectStore(tableName);
-        let getRequest = objectStore.get(id);
+    function update(tableName, id, propriete, updatedData) {
+        let request = window.indexedDB.open("maBaseDeDonnees");
+        request.onsuccess = function (event) {
+            console.log("Ouverture de la base de données");
+            let db = event.target.result;
+            let transaction = db.transaction([tableName], "readwrite");
+            let objectStore = transaction.objectStore(tableName);
+            let getRequest = objectStore.get(id);
 
-        getRequest.onsuccess = function (event) {
-            let record = event.target.result;
-            if (record) {
-                if (propriete === "date_de_naissance") {
-                    let dateNaiss = new Date(updatedData);
-                    record[propriete] = formatDate(dateNaiss);
+            getRequest.onsuccess = function (event) {
+                let record = event.target.result;
+                if (record) {
+                    if (propriete === "date_de_naissance") {
+                        let dateNaiss = new Date(updatedData);
+                        record[propriete] = formatDate(dateNaiss);
+                    } else {
+                        // Effectuer les modifications sur l'enregistrement récupéré
+                        // en utilisant les propriétés de l'objet updatedData
+                        record[propriete] = updatedData;
+                    }
+
+                    // Mettre à jour l'enregistrement dans le magasin d'objets
+                    let updateRequest = objectStore.put(record);
+
+                    updateRequest.onsuccess = function (event) {
+                        const updateEvent = new CustomEvent('updateTextareaEvent', {detail: 'Mise à jour effectuée avec succès'});
+                        window.dispatchEvent(updateEvent);
+                        console.log("Mise à jour effectuée avec succès");
+                    };
+
+                    updateRequest.onerror = function (event) {
+                        const updateEvent = new CustomEvent('updateTextareaEvent', {detail: 'Erreur lors de la mise à jour de l\'enregistrement'});
+                        window.dispatchEvent(updateEvent);
+                        console.log("Erreur lors de la mise à jour de l'enregistrement");
+                    };
                 } else {
-                    // Effectuer les modifications sur l'enregistrement récupéré
-                    // en utilisant les propriétés de l'objet updatedData
-                    record[propriete] = updatedData;
+                    const updateEvent = new CustomEvent('updateTextareaEvent', {detail: 'Enregistrement non trouvé'});
+                    window.dispatchEvent(updateEvent);
+                    console.log("Enregistrement non trouvé");
                 }
+            };
 
-                // Mettre à jour l'enregistrement dans le magasin d'objets
-                let updateRequest = objectStore.put(record);
+            getRequest.onerror = function (event) {
+                console.log("Erreur lors de la récupération de l'enregistrement");
+            };
 
-                updateRequest.onsuccess = function (event) {
-                    const updateEvent = new CustomEvent('updateTextareaEvent', { detail: 'Mise à jour effectuée avec succès' });
-                    window.dispatchEvent(updateEvent);
-                    console.log("Mise à jour effectuée avec succès");
-                };
+            transaction.oncomplete = function (event) {
+                db.close();
+            };
 
-                updateRequest.onerror = function (event) {
-                    const updateEvent = new CustomEvent('updateTextareaEvent', { detail: 'Erreur lors de la mise à jour de l\'enregistrement' });
-                    window.dispatchEvent(updateEvent);
-                    console.log("Erreur lors de la mise à jour de l'enregistrement");
-                };
-            } else {
-                const updateEvent = new CustomEvent('updateTextareaEvent', { detail: 'Enregistrement non trouvé' });
-                window.dispatchEvent(updateEvent);
-                console.log("Enregistrement non trouvé");
-            }
+            transaction.onerror = function (event) {
+                console.log("Erreur de transaction");
+            };
         };
 
-        getRequest.onerror = function (event) {
-            console.log("Erreur lors de la récupération de l'enregistrement");
-        };
-
-        transaction.oncomplete = function (event) {
-            db.close();
-        };
-
-        transaction.onerror = function (event) {
-            console.log("Erreur de transaction");
-        };
-    };
-
-    let oldTable = document.getElementById("tableHtml");
-    if (oldTable) {
-        oldTable.remove();
+        let oldTable = document.getElementById("tableHtml");
+        if (oldTable) {
+            oldTable.remove();
+        }
     }
 }
-
 // Exportez la fonction de mise à jour
 export { update };
