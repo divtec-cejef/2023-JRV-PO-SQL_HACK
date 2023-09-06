749a520b631faeaefac9dbdf84249b681421e8f4
Luca Pinto <pintoluca106@gmail.com>
2023-09-06 10:30:19 +0200

    correction ortographe

--- vue-app/src/pages/barreLaterale/barre-laterale.vue
+++ vue-app/src/pages/barreLaterale/barre-laterale.vue
@@ -6,7 +6,7 @@
   <div class="barre_lateral_gauche">
     <!--        <img class="logo" src="../src/img/logo-sqlhack.png" alt="">-->
     <h1>Comment ça fonctionne ?</h1>
-    <p>Pour interagir avec la base de données, on utilise un langage informatique qui s’appelle le <strong>SQL</strong>.
+    <p>Pour interagir avec la base de données, on utilise un langage informatique qui s’appelle le SQL.
       Ce langage permet d’utiliser 4 commandes pour exécuter des requêtes sur une base de données.</p>
 
     <h2>Les 4 commandes </h2>
@@ -14,9 +14,8 @@
     <p>UPDATE est utilisée pour modifier des données existantes dans votre base de données.</p>
     <p>INSERT est utilisée pour ajouter une nouvelle donnée dans votre base de données
       sous forme d’une nouvelle ligne dans votre tableau.</p>
-    <p>DELETE est utilisée pour supprimer une donnée dans votre base de données</p>
-    <p>L'<strong>ID</strong> est un nombre unique que chaque information possède. Ce numéro est utilisé pour permettre d’identifier une information.</p>
-
+    <p>DELETE est utilisée pour supprimer une donnée dans votre base de données.</p>
+    <p>L'ID est un nombre unique que chaque information possède. Ce numéro est utilisé pour permettre d’identifier une information.</p>
   </div>
 </template>
 
